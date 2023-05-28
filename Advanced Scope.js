81ccb0cd11bb0af3caa903ff0dd8b66252c196db
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-05-28 14:31:10 +0500

    updated

--- Advanced Scope.js
+++ Advanced Scope.js
@@ -1,4 +1,4 @@
-// FUNCTION SCOPE
+// // FUNCTION SCOPE
 const teacher = 'Anwar'; // !!!
 
 function a() {
@@ -15,13 +15,39 @@ console.log(teacher);
 // Ibrohim
 // Adham
 
-// BLOCK SCOPING
-const student = 'Ismail';
-
-{
+// // BLOCK SCOPING
+const student = 'Ismail'; {
 	const student = 'Ishaq';
 	console.log(student)
 }
 
-
 console.log(student)
+
+teacher = 'Sulayman';
+// var teacher;
+
+console.log(teacher)
+
+function getTeacher() {
+	return teacher;
+}
+
+function ask(question) { setTimeout(function waitASec() { console.log(question) }, 1000) }
+ask('savol')
+
+var teacher = 'Kyle';
+var myTeacher = function () {
+	console.log(teacher)
+};
+var teacher = 'Anwar';
+myTeacher(); // Anwar
+
+for (var i = 1; i <= 3; i++) {
+	let j = i;
+	setTimeout(function () {
+		console.log(`j: ${j}`)
+	}, j * 1000);
+};
+// Module Pattern
+var workshop = { teacher: 'Armaan', ask(quiz) { console.log(this.teacher, quiz) } }
+workshop.ask('howdy?')
