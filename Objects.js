4a9c6b88541efe2fd22839e98dc8cac52d06a68d
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-05-29 09:40:24 +0500

    Updated

--- Objects.js
+++ Objects.js
@@ -1,29 +1,29 @@
-// this
-// function ask(question) {
-// 	console.log(this.teacher, question);
-// }
+/// this
+function ask(question) {
+	console.log(this.teacher, question);
+}
 
-// function otherFun() {
-// 	var context = { teacher: "Kareem" };
-// 	ask.call(context, "Why?");
-// }
-// otherFun();
+function otherFun() {
+	var context = { teacher: "Kareem" };
+	ask.call(context, "Why?");
+}
+otherFun();
 
-// class
-// class workshop {
-// 	constructor(teacher) {
-// 		this.teacher = teacher;
-// 	}
-// 	ask(question) {
-// 		console.log(this.teacher, question);
-// 	}
-// };
+/// class
+class workshop {
+	constructor(teacher) {
+		this.teacher = teacher;
+	}
+	ask(question) {
+		console.log(this.teacher, question);
+	}
+};
 
-// var deepJs = new workshop('Majeed');
-// var nodeJs = new workshop('Ali');
+var deepJs = new workshop('Majeed');
+var nodeJs = new workshop('Ali');
 
-// deepJs.ask('Howdy!')
-// nodeJs.ask('Howdy!')
+deepJs.ask('Howdy!')
+nodeJs.ask('Howdy!')
 //
 class workshop {
 	constructor(teacher) {
